acf7267aa5903cc53f16c880c3ac69e7d0926a80
Haodong Huang <cavac1997@gmail.com>
2020-05-14 10:02:10 -0500

    space format

--- Test.java
+++ Test.java
@@ -3,18 +3,18 @@ import java.util.*;
 import edu.stanford.nlp.util.Pair;
 
 public class Test {
-	public static void main(String args[]) throws FileNotFoundException {
-		// initialize configurations with config file path
-		Config.init("./settings.conf");
+    public static void main(String args[]) throws FileNotFoundException {
+        // initialize configurations with config file path
+        Config.init("./settings.conf");
 
-		// TermExtractor.query(String)
-		// Input: a sentence to be queried
-		// Output: a list of pair of extracted terms and corresponding definitions(not simplified)
-		// Support fuzzy matching
-		TermExtractor extractor = new TermExtractor();
-		List<Pair<String, String>> out = extractor.query("DNA vyrus");
-		for(Pair<String, String> x : out) {
-			System.out.println(x);
-		}
-	}
+        // TermExtractor.query(String)
+        // Input: a sentence to be queried
+        // Output: a list of pair of extracted terms and corresponding definitions(not simplified)
+        // Support fuzzy matching
+        TermExtractor extractor = new TermExtractor();
+        List<Pair<String, String>> out = extractor.query("DNA vyrus");
+        for(Pair<String, String> x : out) {
+            System.out.println(x);
+        }
+    }
 }
